7e57086f0ba26a5efbe5cb4fe0a1e9dc8e1bf069
João Victor da Silva <jv782063@gmail.com>
2024-04-25 19:09:50 -0300

    takes the numbers passed in by the user

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,40 @@
+use std::io;
+
 use quicksort as qs;
 
 mod quicksort;
 
 fn main() {
-    let mut to_sort = [9, 491, 23, 434, 533, 2, 28, 98, 1, 37, 68, 1, 355];
+    let mut to_sort = get_user_input_numbers();
+
+    if to_sort.len() == 0 {
+        println!("No data input!...");
+        return;
+    }
 
     qs::sort(&mut to_sort);
     println!("\x1b[93mSorted: {:?}", to_sort);
+}
+
+fn get_user_input_numbers() -> Vec<i32> {
+    println!("Enter the integers one after the other:");
+
+    let mut input_numbers = String::new();
+    io::stdin()
+        .read_line(&mut input_numbers)
+        .expect("Failed to read line!");
+
+    let input_numbers: Vec<i32> = input_numbers
+        .trim()
+        .split_whitespace()
+        .filter_map(|number| match number.trim().parse() {
+            Ok(n) => Some(n),
+            Err(_) => {
+                println!("'{}' is invalid number!", number);
+                None
+            }
+        })
+        .collect();
+
+    input_numbers
 }
